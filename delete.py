db502163fadce1dffa6be6c25bfc823ff74f611b
GeekBoySachin <sachinv1004@gmail.com>
2022-07-18 01:06:52 +0530

    Implementing crud operations in python

--- delete.py
+++ delete.py
@@ -3,5 +3,8 @@ import mysql.connector as conn
 connect = conn.connect(host = "localhost",user = "root", password = "root")
 print(connect)
 cursor = connect.cursor()
+cursor.execute("use sqlclass")
+cursor.execute("drop table bank_details")
+cursor.execute("drop table bank_details1")
 cursor.execute("drop database sqlclass")
 connect.close()
